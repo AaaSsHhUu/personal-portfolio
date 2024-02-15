668f4244f9f8848f3c95ffc03ee45aae4d67e625
Ashu Negi <anegi6723@gmail.com>
2024-02-15 21:03:25 +0530

    Created navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import logo from '../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='bg-gray-200 py-4 px-8 flex flex-col sm:flex-row justify-between items-center container sticky top-0'>
+        <div className='flex items-center'>
+            <img src={logo} className='rounded-full h-16 mr-2' />
+            <h1 className='text-black font-serif italic'>Ashu</h1>
+        </div>
+        <div className='hidden sm:block mr-24'>
+            <ul className='flex gap-4 flex-col justify-center sm:flex-row sm:items-center '>
+              <li><a href="#home" className='text-black text-lg p-2 rounded-lg hover:bg-blue-800 hover:text-white font-medium'>Home</a></li>
+              <li><a href="#skills" className='text-black text-lg p-2 rounded-lg hover:bg-blue-800 hover:text-white font-medium'>Skills</a></li>
+              <li><a href="#about" className='text-black text-lg p-2 rounded-lg hover:bg-blue-800 hover:text-white font-medium'>About</a></li>
+              <li><a href="#contact" className='text-black text-lg p-2 rounded-lg hover:bg-blue-800 hover:text-white font-medium'>Contact Us</a></li>
+            </ul>
+        </div>
+    </section>
   )
 }
 
