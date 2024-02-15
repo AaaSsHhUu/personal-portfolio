b3cd295c5c738c9f3f751a5333167e7357fc18d7
Ashu Negi <anegi6723@gmail.com>
2024-02-15 16:56:56 +0530

    imported all  components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import About from './components/About'
+import Project from './components/Project'
+import Skills from './components/Skills'
+import Contact from './components/Contact'
+
 
 function App() {
   return (
-    <>
-
-    </>
+    <main>
+        <Navbar />
+        <About />
+        <Project />
+        <Skills />
+        <Contact />
+    </main>
   )
 }
 
