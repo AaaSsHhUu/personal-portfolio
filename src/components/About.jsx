371762e01a3ffb87a3dc32b9b2a6e1a1cc2dbe1b
Ashu Negi <anegi6723@gmail.com>
2024-02-18 12:16:50 +0530

    modifying About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
 import svg from '../assets/react.svg';
+import Next from './Next';
 
 const About = () => {
   return (
-    <section id='about' className='min-h-screen w-full flex flex-col items-center sm:flex-row flex-1'>
-      {/* Left side of About section */}
-        <div className=' w-2/4 px-28 py-20'>
-            <h1 id='intro' className=' text-white font-roboto text-5xl leading-snug'>
-              Welcome to my Portfolio<br /> I'm a
-              <br /> 
-              <span className=' text-blue-400 font-bold text-6xl'>React Developer</span>
-            </h1>
+    <section id='about' className='h-80vh pb-12 w-full flex flex-col'>
+        <div className='flex flex-col md:flex-row items-center w-full'>
+          {/* Left side of About section */}
+          <div className=' w-3/6 px-28 py-20'>
+              <h1 id='intro' className=' text-white font-roboto text-5xl leading-snug'>
+                Welcome to my Portfolio<br /> I'm a
+                <br /> 
+                <span className=' text-react font-bold text-6xl'>React Developer</span>
+              </h1>
+
+              <div>
+                <button className='px-4 py-2 text-white text-lg font-bold hover:text-black bg-blue-400 mt-8'>Resume</button>
+              </div>
            
+          </div>
+          {/* Right side of About section */}
+          <div className='w-4/12 ml-6 flex items-center animate-spin justify-center'>
+              <img src={svg} className='w-3/4'/>
+          </div>
         </div>
-        {/* Right side of About section */}
-        <div className='w-5/12 flex items-center animate-spin justify-center'>
-            <img src={svg} className='w-2/4'/>
+
+        <div className='flex justify-center w-full'>
+          <Next content={"projects"}/>
         </div>
     </section>
   )
