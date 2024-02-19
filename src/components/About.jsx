f74e8ff0d06bde23f2c4b14f61222e85b7eed171
Ashu Negi <anegi6723@gmail.com>
2024-02-19 15:51:54 +0530

    made about section responsive for mobile devices

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,28 +4,28 @@ import Next from './Next';
 
 const About = () => {
   return (
-    <section id='about' className='h-80vh pb-12 w-full flex flex-col'>
-        <div className='flex flex-col md:flex-row items-center w-full'>
+    <section id='about' className='min-h-screen w-full flex flex-col'>
+        <div className='flex flex-col-reverse md:flex-row items-center w-full'>
           {/* Left side of About section */}
-          <div className=' w-3/6 px-28 py-20'>
-              <h1 id='intro' className=' text-white font-roboto text-5xl leading-snug'>
+          <div className=' sm:w-3/6 sm:px-28 sm:py-20 text-center'>
+              <h1 id='intro' className='py-4 text-white font-roboto text-4xl md:text-5xl leading-snug'>
                 Welcome to my Portfolio<br /> I'm a
                 <br /> 
-                <span className=' text-react font-bold text-6xl'>React Developer</span>
+                <span className='leading-relaxed text-react font-bold text-5xl md:text-6xl'>React Developer</span>
               </h1>
 
               <div>
-                <button className='px-4 py-2 text-white text-lg font-bold hover:text-black bg-blue-400 mt-8'>Resume</button>
+                <button className='px-4 py-2 text-2xl text-white sm:text-lg font-bold hover:text-black bg-blue-400 mt-8'>Resume</button>
               </div>
            
           </div>
           {/* Right side of About section */}
-          <div className='w-4/12 ml-6 flex items-center animate-spin justify-center'>
+          <div className='w-2/4 py-14 md:w-4/12 flex items-center animate-spin justify-center'>
               <img src={svg} className='w-3/4'/>
           </div>
         </div>
 
-        <div className='relative -bottom-7 flex justify-center w-full'>
+        <div className='absolute bottom-0 flex justify-center w-full'>
           <Next content={"projects"}/>
         </div>
     </section>
